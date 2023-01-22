c91bab5b8bd582c2a801d0c7e0e4d4f1f0357266
manato-alt <cococara988@gmail.com>
2023-01-22 11:25:52 +0900

    重複した（使わない）ルーティングを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'relationships/create'
-  get 'relationships/destroy'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   get 'events/index'
   root 'events#index'
